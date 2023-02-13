ba0a94f559ad12de5a5bac442171059ea65efe30
Tristan-Boudra <boudratristan@gmail.com>
2023-02-13 15:05:58 +0100

    feat(query): first query

--- src/profil/profil.controller.ts
+++ src/profil/profil.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Controller('profil')
 export class ProfilController {
-  // constructor(@InjectDataSource() private bdd: DataSource) {}
+  constructor(@InjectDataSource() private bdd: DataSource) {}
 
   @Post()
   create(@Body() createProfilDto) {
+    this.bdd.query("INSERT INTO profil (nom, prenom, email, password) VALUES (?, ?, ?, ?)", [createProfilDto.nom, createProfilDto.prenom, createProfilDto.email, createProfilDto.password]);
     return "Todo";
   }
 
--- src/profil/profil.module.ts
+++ src/profil/profil.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ProfilService } from './profil.service';
 import { ProfilController } from './profil.controller';
 
 @Module({
   controllers: [ProfilController],
-  providers: [ProfilService]
+  providers: []
 })
 export class ProfilModule {}
